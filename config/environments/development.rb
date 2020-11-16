dea6d7ac652117d5eaee746142b4ea3ae364c7fb
Oasist <bin.lhkty.gtthj.o.zs@gmail.com>
2020-11-16 12:11:18 +0900

    Change config.file_watcher

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -57,6 +57,7 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
 
   # Use an evented file watcher to asynchronously detect changes in source code,
-  # routes, locales, etc. This feature depends on the listen gem.
-  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # Enable instant reflection of diffs in Docker environment
+    config.file_watcher = ActiveSupport::FileUpdateChecker
 end
